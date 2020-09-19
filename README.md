0b382b5180ad52e01cc8106af0545374048b6d04
John Fay <john.k.fay@gmail.com>
2020-09-19 01:30:09 -0400

    docs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Building a Typescript script 101
+
+Article: [https://johnfay.dev/blog/setting-up-scripts-with-typescript/]()
+
+## What is covered
+
+- Setting up babel to run Javascript file
+- Setting up babel to run Typescript file
+- Fetching API response using `node-fetch`
+- Generating Interfaces and reducing duplication using the Omit operator
+- Writing to file using node file system
--- index.ts
+++ index.ts
@@ -1,1 +1,51 @@
-console.log("hello world");
+import fetch from "node-fetch";
+import { writeFileSync } from "fs";
+
+interface USInputDay {
+  date: Date;
+  states: number;
+  positive: number;
+  negative: number;
+  pending: number;
+  hospitalizedCurrently: number;
+  hospitalizedCumulative: number;
+  inIcuCurrently: number;
+  inIcuCumulative: number;
+  onVentilatorCurrently: number;
+  onVentilatorCumulative: number;
+  recovered: number;
+  dateChecked: Date;
+  death: number;
+  hospitalized: number;
+  lastModified: Date;
+  total: number;
+  totalTestResults: number;
+  posNeg: number;
+  deathIncrease: number;
+  hospitalizedIncrease: number;
+  negativeIncrease: number;
+  positiveIncrease: number;
+  totalTestResultsIncrease: number;
+  hash: string;
+}
+
+interface USOutputDay {
+  x: Date;
+  y: Omit<USInputDay, "date" | "dateChecked" | "lastModified" | "hash">;
+}
+
+(async () => {
+  const response = await fetch(
+    "https://api.covidtracking.com/v1/us/daily.json"
+  );
+  const json: USInputDay[] = await response.json();
+
+  const output: USOutputDay[] = json.map(
+    ({ date, dateChecked, lastModified, hash, ...theRest }) => ({
+      x: date,
+      y: theRest,
+    })
+  );
+
+  writeFileSync("formatted.json", JSON.stringify(output));
+})();
